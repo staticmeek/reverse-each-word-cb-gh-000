846d6bae312b66035a72f37670229fa6bc7dd35d
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-08 00:13:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 def reverse_each_word(strings)
-  words = strings.split(" ")
-  strings = [words]
-  strings.each do |string|
-    string.reverse
-    string << strings
-  end
-  strings
+  words = [strings.split(" ")]
+  words.each do |word|
+    word.reverse << words
+
 end
